707ab84e5fabc6891bb7a60c7254a0f06c0729a0
Amanda Hinchman-Dominguez <amanda.hinchman-dominguez@yello.co>
2019-03-11 23:43:03 -0500

    added images to the movie data class

--- app/src/main/java/com/example/amandahinchman_dominguez/recyclerviews/MainFragment.kt
+++ app/src/main/java/com/example/amandahinchman_dominguez/recyclerviews/MainFragment.kt
@@ -8,19 +8,19 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_main.*
 
-data class Movie(val title: String, val year: Int)
+data class Movie(val title: String, val year: Int, val image: String)
 
 class MainFragment : Fragment() {
 
     private val nicCageMovies = listOf(
-        Movie("Raising Arizona", 1987),
-        Movie("Vampire's Kiss", 1988),
-        Movie("Con Air", 1997),
-        Movie("Face/Off", 1997),
-        Movie("National Treasure", 2004),
-        Movie("The Wicker Man", 2006),
-        Movie("Bad Lieutenant", 2009),
-        Movie("Kick-Ass", 2010)
+        Movie("Raising Arizona", 1987, "raising_arizona.jpg"),
+        Movie("Vampire's Kiss", 1988, "vampires_kiss.png"),
+        Movie("Con Air", 1997, "con_air.jpg"),
+        Movie("Face/Off", 1997, "face_off.jpg"),
+        Movie("National Treasure", 2004, "national_treasure.jpg"),
+        Movie("The Wicker Man", 2006, "wicker_man.jpg"),
+        Movie("Bad Lieutenant", 2009, "bad_lieutenant.jpg"),
+        Movie("Kick-Ass", 2010, "kickass.jpg")
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
